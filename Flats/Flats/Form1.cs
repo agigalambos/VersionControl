b616f0d6c3fb3d5b064642c4edbe0d2dafdac1ba
Ági Galambos <galambos.agi@gmail.com>
2020-10-03 15:40:38 +0200

    Adatok betöltése

--- Flats/Flats/Form1.cs
+++ Flats/Flats/Form1.cs
@@ -4,18 +4,29 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Flats
 {
+    
     public partial class Form1 : Form
     {
-        
+        RealEstateEntities context = new RealEstateEntities();
+
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+
     }
 }
