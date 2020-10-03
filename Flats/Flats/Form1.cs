efb70da7123821877d78971ad9fcaf1707928e20
Ági Galambos <galambos.agi@gmail.com>
2020-10-03 17:08:06 +0200

    Excel munkafüzet létrehozása és megnyitása

--- Flats/Flats/Form1.cs
+++ Flats/Flats/Form1.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Data.Entity.Migrations.Model;
 
 namespace Flats
 {
@@ -20,15 +21,52 @@ namespace Flats
 
         List<Flat> Flats;
 
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
+            CreateExcel();
         }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
         }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true; 
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false,Type.Missing,Type.Missing );
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        
+        
+        
+        }
+
+
+
+
 
     }
 }
