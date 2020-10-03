6627d59d625bf4a9a5694bec0ad758009dcfcd06
Ági Galambos <galambos.agi@gmail.com>
2020-10-03 15:43:59 +0200

    Excel eszközök behivatkozása

--- Flats/Flats/Form1.cs
+++ Flats/Flats/Form1.cs
@@ -8,6 +8,8 @@ using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace Flats
 {
