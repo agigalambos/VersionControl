1e033fdee62f7d48d47efb201d28bef59bb643b7
Ági Galambos <galambos.agi@gmail.com>
2020-10-03 18:34:07 +0200

    Kimenet formázása

--- Flats/Flats/Form1.cs
+++ Flats/Flats/Form1.cs
@@ -15,7 +15,7 @@ using System.Drawing.Text;
 
 namespace Flats
 {
-    
+
     public partial class Form1 : Form
     {
         RealEstateEntities context = new RealEstateEntities();
@@ -49,21 +49,21 @@ namespace Flats
                 CreateTable();
 
                 xlApp.Visible = true;
-                xlApp.UserControl = true; 
+                xlApp.UserControl = true;
             }
             catch (Exception ex)
             {
                 string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
                 MessageBox.Show(errMsg, "Error");
 
-                xlWB.Close(false,Type.Missing,Type.Missing );
+                xlWB.Close(false, Type.Missing, Type.Missing);
                 xlApp.Quit();
                 xlWB = null;
                 xlApp = null;
             }
-        
-        
-        
+
+
+
         }
         private string GetCell(int x, int y)
         {
@@ -81,8 +81,8 @@ namespace Flats
 
             return ExcelCoordinate;
         }
-         //
-        private void CreateTable() 
+        //
+        private void CreateTable()
         {
             string[] headers = new string[]
                 {
@@ -119,27 +119,39 @@ namespace Flats
                 values[counter, 5] = f.NumberOfRooms;
                 values[counter, 6] = f.FloorArea;
                 values[counter, 7] = f.Price;
-                values[counter, 8] = "=" + GetCell(counter + 2, 7) + "*" + GetCell(counter + 2, 8); 
+                values[counter, 8] = "=" + GetCell(counter + 2, 7) + "*" + GetCell(counter + 2, 8);
                 counter++;
             }
 
-            
+
 
             xlSheet.get_Range(
              GetCell(2, 1),
              GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
 
 
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
 
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
 
+            Excel.Range tableRange = xlSheet.get_Range(GetCell(2,1),GetCell(lastRowID,headers.Length));
+            tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range firstColumnRange = xlSheet.get_Range(GetCell(2,1),GetCell(lastRowID,1));
+            firstColumnRange.Font.Bold = true;
+            firstColumnRange.Interior.Color = Color.LightYellow;
+
+            Excel.Range lastColumnRange = xlSheet.get_Range(GetCell(2, headers.Length), GetCell(lastRowID, headers.Length));
+            lastColumnRange.Interior.Color = Color.LightGreen;
+            lastColumnRange.NumberFormat = "#,##0.00";
         }
-
-            
-
-
-
-
-        }
-
     }
+}
 
