481a026d4b228d86a919a55b1fe5a9367b25c5af
Ági Galambos <galambos.agi@gmail.com>
2020-11-11 19:13:01 +0100

    Absztrakció felhasználása a gyárakban

--- SantaClaus_Factory/SantaClaus_Factory/Entities/CarFactory.cs
+++ SantaClaus_Factory/SantaClaus_Factory/Entities/CarFactory.cs
@@ -7,12 +7,11 @@ using System.Threading.Tasks;
 
 namespace SantaClaus_Factory.Entities
 {
-    public class BallFactory:IToyFactory
+    public class CarFactory:IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
-
     }
 }
