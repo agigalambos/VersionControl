86d0fcaa01a27b7572d29f3c73fd0dc91be04866
Ági Galambos <galambos.agi@gmail.com>
2020-11-24 10:39:36 +0100

    A játék kipróbálása

--- EvolutionaryAlgorithm/EvolutionaryAlgorithm/Form1.cs
+++ EvolutionaryAlgorithm/EvolutionaryAlgorithm/Form1.cs
@@ -22,6 +22,9 @@ namespace EvolutionaryAlgorithm
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
